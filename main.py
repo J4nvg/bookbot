9d5bc245f7598a7cb0378f38a50d699ed9862893
J4nvg <janvgestel@outlook.com>
2024-10-19 21:35:44 +0200

    I am done

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+FRANK = "books/frankenstein.txt"
 def readBook(filename):
     with open(filename) as f:
         for line in f:
             print(line)
 
+def countWords(filename):
+    count = 0
+    with open(filename) as f:
+        for line in f:
+            count += len(line.split())
+    return count
+
+def charCount(filename):
+    ndict ={}
+    with open(filename) as f:
+        for line in f:
+            for word in line.strip().split():
+                for char in word:
+                    if ord(char.lower()) >= 97 and ord(char.lower()) <= 122:
+                        ndict[char.lower()] = ndict.get(char.lower(),0) + 1
+    return ndict
+
 if __name__ == "__main__":
-    readBook("books/frankenstein.txt")
+    # readBook(FRANK)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{countWords(FRANK)} words found in the document.\n")
+    ndict = charCount(FRANK)
+    keylist = list(ndict.keys())
+    keylist.sort()
+    for key in keylist:
+        print(f"the '{key}' character was found {ndict[key]} times.\n")
